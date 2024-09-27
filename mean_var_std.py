9d655b454c83b31ab49f9eb79098df17a0122251
Gabriela Silva <gabriela.silva2@sou.inteli.edu.br>
2024-09-27 18:14:58 +0000

    atividade 1

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
-def calculate(list):
+def calculate(input_list):
+    # Verificar se a lista contém exatamente 9 números
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
-
-
-
+    # Converter a lista em um array Numpy 3x3
+    matrix = np.array(input_list).reshape(3, 3)
+    
+    # Calcular as estatísticas
+    calculations = {
+        'mean': [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()],
+        'variance': [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()],
+        'standard deviation': [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()],
+        'max': [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()],
+        'min': [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()],
+        'sum': [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()]
+    }
+    
     return calculations
